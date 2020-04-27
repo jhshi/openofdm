abbe9ecde9e08267386fd7263707c9a9557df4c3
Xianjun Jiao <putaoshu@msn.com>
2020-04-27 15:46:16 +0200

    extend support to zcu102/Zynq MPSoC ultra_scale

--- verilog/divider.v
+++ verilog/divider.v
@@ -1,5 +1,7 @@
 /*
-* DELAY: 36 cycles
+* xianjun.jiao@imec.be; putaoshu@msn.com
+* DELAY: 36 cycles -- this is old parameter
+* The new div_gen 5.x allow the valid signal, auto delay or manual delay config
 */
 module divider (
     input clock,
@@ -14,18 +16,28 @@ module divider (
     output output_strobe
 );
 
-div_gen_v3_0 div_inst (
+div_gen div_inst (
     .clk(clock),
     .dividend(dividend),
     .divisor(divisor),
+    .input_strobe(input_strobe),
+    .output_strobe(output_strobe),
     .quotient(quotient)
 );
 
-delayT #(.DATA_WIDTH(1), .DELAY(36)) out_inst (
-    .clock(clock),
-    .reset(reset),
-    .data_in(input_strobe),
-    .data_out(output_strobe)
-);
+// // --------old one---------------
+// div_gen_v3_0 div_inst (
+//     .clk(clock),
+//     .dividend(dividend),
+//     .divisor(divisor),
+//     .quotient(quotient)
+// );
+
+// delayT #(.DATA_WIDTH(1), .DELAY(36)) out_inst (
+//     .clock(clock),
+//     .reset(reset),
+//     .data_in(input_strobe),
+//     .data_out(output_strobe)
+// );
 
 endmodule
